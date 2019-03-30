e3b06e635514fb8f8e03d579616c958445e8c117
Nataliia Bilan <natali_bilan@ukr.net>
2019-03-30 17:57:50 +0200

    initial commit

---
+++ build/js/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var buttonOpen = document.querySelector('.btn-open');
+var modalWindow = document.querySelector('.modal');
+var buttonClose = document.querySelector('.btn-close__icon');
+var buttonCancel = document.querySelector('.btn-cancel');
+var buttonUninstall = document.querySelector('.btn-uninstall');
+
+function handleOpenWindow(event) {
+  event.preventDefault();
+  modalWindow.classList.add('modal--open');
+}
+
+buttonOpen.addEventListener('click', handleOpenWindow);
+
+function handleCloseWindow(event) {
+  event.preventDefault();
+  console.log(event.target);
+
+  if (event.target === buttonClose || event.target === modalWindow || event.target === buttonCancel // event.target === buttonUninstall
+  ) {
+      modalWindow.classList.remove('modal--open');
+    }
+}
+
+modalWindow.addEventListener('click', handleCloseWindow);
+
+function handleUninstall(event) {
+  event.preventDefault();
+  modalWindow.classList.remove('modal--open');
+
+  if (modalWindow.classList !== 'modal--open') {
+    alert('Done!');
+  }
+}
+
+buttonUninstall.addEventListener('click', handleUninstall);
---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+'use strict';
+const buttonOpen = document.querySelector('.btn-open');
+const modalWindow = document.querySelector('.modal');
+const buttonClose = document.querySelector('.btn-close__icon');
+const buttonCancel = document.querySelector('.btn-cancel');
+const buttonUninstall = document.querySelector('.btn-uninstall');
+
+function handleOpenWindow(event) {
+	event.preventDefault();
+	modalWindow.classList.add('modal--open');
+}
+buttonOpen.addEventListener('click', handleOpenWindow);
+
+function handleCloseWindow(event) {
+	event.preventDefault();
+	console.log(event.target);
+	if (
+		event.target === buttonClose ||
+		event.target === modalWindow ||
+		event.target === buttonCancel
+		// event.target === buttonUninstall
+	) {
+		modalWindow.classList.remove('modal--open');
+	}
+}
+modalWindow.addEventListener('click', handleCloseWindow);
+
+function handleUninstall(event) {
+	event.preventDefault();
+	modalWindow.classList.remove('modal--open');
+	if (modalWindow.classList !== 'modal--open') {
+		alert('Done!');
+	}
+}
+buttonUninstall.addEventListener('click', handleUninstall);
